a2063bf1ac7cb6e3e608c9ba23418c0a039bc589
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-04-21 15:14:52 +0200

    first commit

---
+++ Wishlists/Wishlists/WishlistsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistsApp.swift
+//  Wishlists
+//
+//  Created by Marianne Indrebø on 21/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct WishlistsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
