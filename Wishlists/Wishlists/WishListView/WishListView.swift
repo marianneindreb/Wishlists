6169da5e0dd53864df671cbd06b44296766f84e1
Marianne <91476569+marianneindreb@users.noreply.github.com>
2024-04-23 20:00:11 +0200

    feat: added new product view and set as sheet in wishlistview (#3)

--- Wishlists/Wishlists/WishListView/WishListView.swift
+++ Wishlists/Wishlists/WishListView/WishListView.swift
@@ -12,13 +12,43 @@ struct WishListView: View {
     var list: ListModel
     var product: ProductItem
     
+    @State private var showAddNewProduct = false
+    
     var body: some View {
-        NavigationStack {
-           ProductCardView(product: product)
+        ZStack{
+            ProductCardView(product: product)
+            Spacer()
+            
+            VStack {
+                Spacer()
+                HStack {
+                    Spacer()
+                    Button {
+                        showAddNewProduct = true
+                        
+                    }label: {
+                        ZStack {
+                            Circle()
+                                .frame(width: 80)
+                                .foregroundStyle(.bg)
+                                .shadow(radius: 5)
+                            Image(systemName: "plus")
+                                .foregroundStyle(.black)
+                                .font(.subheadline)
+                                .fontWeight(.bold)
+                            
+                        }
+                    }
+                    .padding()
+                }
             }
         }
+        .navigationTitle("Wishlist")
+        .sheet(isPresented: $showAddNewProduct, content: {
+            AddNewProductDetailView()
+        })
     }
-
+}
     
         
         
